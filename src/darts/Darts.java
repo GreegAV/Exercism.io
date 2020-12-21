b7c22200e67530af31bfc0a77f480fc409e28340
Андрей Григорьев <greegav@gmail.com>
2020-12-21 15:44:21 +0200

    armstrong darts greeter

---
+++ src/darts/Darts.java
@@ -0,0 +1,23 @@
+package darts;
+
+class Darts {
+    double radius;
+
+    Darts(double x, double y) {
+//        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.radius = Math.sqrt(x * x + y * y);
+    }
+
+    int score() {
+        if (radius <= 1)
+            return 10;
+        else if (radius<=5)
+            return 5;
+        else if (radius<=10)
+            return 1;
+        else
+            return 0;
+
+    }
+
+}

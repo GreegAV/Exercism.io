a226f0c41bfbfc0cce0a1fa248eb82c0bf7ddefb
Андрей Григорьев <greegav@gmail.com>
2020-12-21 16:07:10 +0200

    cleanup

---
+++ src/main/java/leap/Leap.java
@@ -0,0 +1,10 @@
+package leap;
+
+class Leap {
+
+    boolean isLeapYear(int year) {
+//        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return !(year % 4 != 0 || year % 100 == 0 && year % 400 != 0);
+    }
+
+}

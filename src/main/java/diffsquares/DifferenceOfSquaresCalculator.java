8a0ea82890b96312dc4d281bda0afcf19b34f7d4
Андрей Григорьев <greegav@gmail.com>
2020-12-24 12:57:26 +0200

    DiffSquares v2

--- src/main/java/diffsquares/DifferenceOfSquaresCalculator.java
+++ src/main/java/diffsquares/DifferenceOfSquaresCalculator.java
@@ -2,24 +2,32 @@ package diffsquares;
 
 class DifferenceOfSquaresCalculator {
 
-    int computeSquareOfSumTo(int input) {
-        int summ=0;
-        for (int i = 0; i <input+1 ; i++) {
-            summ+=i;
-        }
-        return summ*summ;
+    int computeDifferenceOfSquares(int input) {
+        return computeSquareOfSumTo(input) - computeSumOfSquaresTo(input);
     }
 
     int computeSumOfSquaresTo(int input) {
-        int summ=0;
-        for (int i = 0; i <input+1 ; i++) {
-            summ+=i*i;
+        return (int) (2 * Math.pow(input, 3) + 3 * Math.pow(input, 2) + input) / 6;
+    }
+
+    int computeSquareOfSumTo(int input) {
+        return (int) Math.pow(input * (input + 1) / 2, 2);
+    }
+
+
+    int computeSquareOfSumTo_Old(int input) {
+        int summ = 0;
+        for (int i = 0; i < input + 1; i++) {
+            summ += i;
+        }
+        return summ * summ;
+    }
+
+    int computeSumOfSquaresTo_Old(int input) {
+        int summ = 0;
+        for (int i = 0; i < input + 1; i++) {
+            summ += i * i;
         }
         return summ;
     }
-
-    int computeDifferenceOfSquares(int input) {
-        return computeSquareOfSumTo(input)-computeSumOfSquaresTo(input);
-    }
-
 }
